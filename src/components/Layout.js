01176d62f71f7f1ddc13e318513ddadaa6e3d74d
DevdanRomulo <devdan.romulo.iics@ust.edu.ph>
2022-03-27 16:35:43 +0800

    Layout, Instructions, Card Components

---
+++ src/components/Layout.js
@@ -0,0 +1,9 @@
+const Layout = ({ children }) => {
+    return (
+      <div class="d-flex flex-column min-vh-100 bg-black justify-content-center align-items-center">
+        {children}
+      </div>
+    );
+  };
+  
+  export default Layout;
