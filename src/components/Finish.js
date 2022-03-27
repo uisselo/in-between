4523a7e79626065d9f4f9aad740b3d8cb2bb1912
louisse <louisse.evale.iics@ust.edu.ph>
2022-03-28 01:27:11 +0800

    5pts score congratulations msg and confetti

--- src/components/Finish.js
+++ src/components/Finish.js
@@ -1,31 +1,45 @@
 import { Link, useLocation } from "react-router-dom";
+import Confetti from "react-confetti";
 import Card from "./Card";
 
 const Finish = () => {
   const location = useLocation();
   const { totalScore } = location.state;
   return (
-    <Card title="Done!">
-      <p>
-        Thank you for playing! Your total score:{" "}
-        <b class="text-white fw-normal fs-5">{totalScore}</b>
-        <br />
-        {totalScore === 5 ? <p>Congratulations! You won all rounds!</p> : null}
-      </p>
+    <>
+      {totalScore === 5 ? <Confetti /> : null}
+      <Card title="Done!">
+        <p>
+          {totalScore === 5 ? (
+            <>
+              <p>
+                Congratulations! You won all rounds with{" "}
+                <b class="text-white fw-normal fs-5">{totalScore}</b> points.
+                Thank you for playing!
+              </p>
+            </>
+          ) : (
+            <>
+              Thank you for playing! Your total score:{" "}
+              <b class="text-white fw-normal fs-5">{totalScore}</b>
+            </>
+          )}
+        </p>
 
-      <Link
-        to="/"
-        type="button"
-        class="btn btn-lg rounded-0 text-white text-uppercase"
-      >
-        Play Again →
-      </Link>
-      <p class="mb-0 mt-3 text-end text-uppercase creators">
-        Designed by Louisse Evale and Devdan Romulo
-        <br />
-        4ITC - UST CICS
-      </p>
-    </Card>
+        <Link
+          to="/"
+          type="button"
+          class="btn btn-lg rounded-0 text-white text-uppercase"
+        >
+          Play Again →
+        </Link>
+        <p class="mb-0 mt-3 text-end text-uppercase creators">
+          Designed by Louisse Evale and Devdan Romulo
+          <br />
+          4ITC - UST CICS
+        </p>
+      </Card>
+    </>
   );
 };
 
--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,5 +1,5 @@
-import Card from "./Card";
 import { Link } from "react-router-dom";
+import Card from "./Card";
 
 const Instructions = () => {
   return (
